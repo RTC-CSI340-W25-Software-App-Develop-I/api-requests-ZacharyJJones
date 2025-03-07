eb2c6fc16b2c729a74c9d3e380a9e589918cb155
Zack Jones <81715488+ZacharyJJones@users.noreply.github.com>
2025-03-06 21:42:31 -0800

    lab complete

--- server/index.js
+++ server/index.js
@@ -1,3 +1,34 @@
-import { restaurants } from "data";
+import { restaurants } from "./data.js";
+import express from "express";
+import cors from "cors";
 
 const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+// ===========
+
+app.get("/restaurants", (req, res) => {
+	return res.json(restaurants);
+});
+
+app.post("/restaurants", (req, res) => {
+	const newRestaurant = {
+		name: req.body.name,
+		address: req.body.address,
+		phone: req.body.phone,
+		cuisine: req.body.cuisine,
+		rating: req.body.rating,
+		hours: req.body.hours,
+		menu: req.body.menu,
+	};
+
+	restaurants.push(newRestaurant);
+	res.status(201).json(newRestaurant);
+});
+
+app.listen(port, () => {
+	console.log(`Server running on http://localhost:${port}`);
+});
